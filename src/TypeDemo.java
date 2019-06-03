665c1f92292a08252b9af9388abb11ed2fa3b58d
Liam Chan-Wicht <liamchanwicht@hotmail.com>
2019-06-03 21:02:16 +1000

    first commit. unfinished code.

---
+++ src/TypeDemo.java
@@ -0,0 +1,27 @@
+
+public class TypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte hoursIneclipse;
+		hoursIneclipse=127;
+		
+		short coffeesDrank;
+		coffeesDrank=32767;
+		
+		int codeErrors;
+		codeErrors=2147483647;
+		
+		long hairsPulled;
+		hairsPulled = 9223372036854775806;
+				
+
+		System.out.println(hoursIneclipse);
+		System.out.println(coffeesDrank);
+		System.out.println(codeErrors);
+		
+		
+	}
+
+}

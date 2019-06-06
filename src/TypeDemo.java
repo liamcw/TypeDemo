a590e2046b9c9c9f2d1ab7a6bfff1de38d2600fd
Liam Chan-Wicht <liamchanwicht@hotmail.com>
2019-06-06 17:38:43 +1000

    changed values to MAX_VALUE to check previous manually entered values.

--- src/TypeDemo.java
+++ src/TypeDemo.java
@@ -5,21 +5,21 @@ public class TypeDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		byte hoursIneclipse = 127;
+		byte hoursIneclipse = Byte.MAX_VALUE;
 		// The byte data type is an 8-bit signed two's complement integer. It has a
 		// minimum value of -128 and a maximum value of 127 (inclusive).
-		short coffeesDrank = 32767;
+		short coffeesDrank = Short.MAX_VALUE;
 		// The short data type is a 16-bit signed two's complement integer. It has a
 		// minimum value of -32,768 and a maximum value of 32,767 (inclusive).
-		int codeErrors = 2147483647;
+		int codeErrors = Integer.MAX_VALUE;
 		// By default, the int data type is a 32-bit signed two's complement integer,
 		// which has a minimum value of -231 and a maximum value of 231-1.
-		long hairsPulled = 9223372036854775806L;
+		long hairsPulled = Long.MAX_VALUE;
 		// The long data type is a 64-bit two's complement integer. The signed long has
 		// a minimum value of -263 and a maximum value of 263-1.
-		float floatingPoints = 340282346638528860000000000000000000000.000000F;
+		float floatingPoints = Float.MAX_VALUE;
 		// The float data type is a single-precision 32-bit IEEE 754 floating point.
-		double hoursSincelastMeal = 2 ^ 53;
+		double hoursSincelastMeal = Double.MAX_VALUE;
 		// Double data type is used to store decimal numbers whose range is from 1.7
 		// E-308 to 1.7 E+308.
 		boolean gonnaLetyouDown = false;
@@ -34,6 +34,21 @@ public class TypeDemo {
 				+ gonnaLetyouDown + "\n" + "What's The First Letter of the Alphabet: " + abc + "\n";
 
 		JOptionPane.showMessageDialog(null, message);
+
+		hoursIneclipse++; 
+		coffeesDrank++;
+		codeErrors++;
+		hairsPulled++;
+		floatingPoints++;
+		hoursSincelastMeal++;
+		//incrementing the existing values of the number variables by 1
+
+		String message2 = "Hours in Eclipse: " + hoursIneclipse + "\n" + "Coffees Drank: " + coffeesDrank++ + "\n"
+				+ "Code Errors: " + codeErrors + "\n" + "Hairs Pulled: " + hairsPulled + "\n" + "Floating Points: "
+				+ floatingPoints + "\n" + "Hours Since Last Meal: " + hoursSincelastMeal + "\n" + "Gonna Let You Down: "
+				+ gonnaLetyouDown + "\n" + "What's The First Letter of the Alphabet: " + abc + "\n";
+
+		System.out.println(message2);
 
 	}
 
